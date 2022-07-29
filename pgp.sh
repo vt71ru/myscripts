0218bd4288ff72055b5b312876d6b1ee3ab6307c
vt71ru <69967509+vt71ru@users.noreply.github.com>
2022-07-29 13:15:08 +0300

    Update pgp.sh

--- pgp.sh
+++ pgp.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env  bash
 
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys EAC0D406E5D79A82ADEEDFDFB76E53652D87398A
-wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb && dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 2CC26F777B8B44A1
-wget -nc https://dl.winehq.org/wine-builds/winehq.key && apt-key add winehq.key && rm winehq.key
+echo -e "Sublime text........."
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
