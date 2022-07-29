c06b92ea64ab56ec5329195896ac18df940530bd
vt71ru <69967509+vt71ru@users.noreply.github.com>
2022-07-29 15:11:41 +0300

    Update pgp.sh

--- pgp.sh
+++ pgp.sh
@@ -3,3 +3,5 @@
 echo -e "Sublime text........."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+apt-get update
+apt-get upgrade
